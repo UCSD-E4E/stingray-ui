538ac725ca0c82bbb7ddef121cbd973036f4fe1a
awilby <antonellawilby@gmail.com>
2013-12-26 16:37:16 -0800

    beginning of user interface definition for mission controller

--- src/mission_controller/src/mission_controller/mission_controller.py
+++ src/mission_controller/src/mission_controller/mission_controller.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from argparse import ArgumentParser
+import argparse
 from qt_gui.plugin import Plugin
 from .mission_controller_widget import MissionControllerWidget
 
@@ -11,7 +11,7 @@ class MissionController(Plugin):
 
     def __init__(self, context):
         """ Initializes widget and UI.
-                :param context: plugin context hook to enable adding widgets as a ROS Gui pane, 'PluginContext'
+                :param context: plugin context hook to enable adding widgets as a ROS_Gui pane, 'PluginContext'
         """
 
         # Get parent class
@@ -22,20 +22,20 @@ class MissionController(Plugin):
         self._widget = MissionControllerWidget(context)
 
         # For case when there are more than 1 of the widget in the rqt window
-        if context.serial_number > 1:
+        if context.serial_number() > 1:
             self._widget.setWindowTitle(self._widget.windowTitle() + (' (%d)' % context.serial_number()))
         
         # Add widgets to user interface
         context.add_widget(self._widget)
 
         # Parse command line arguments
-        args = self.parse_args(context.argv())
+        args = self._parse_args(context.argv())
 
 
-    def parse_args(self, argv):
+    def _parse_args(self, argv):
         """ Parses command line arguments. """
 
-        parser = ArgumentParser(prog="mission_controller", add_help=False)
+        parser = argparse.ArgumentParser(prog="mission_controller", add_help=False)
         MissionController.add_arguments(parser)
         return parser.parse_args(argv)
 
@@ -45,7 +45,7 @@ class MissionController(Plugin):
         """..."""
 
         group = parser.add_argument_group('Options for mission_controller plugin')
-        #group.add_argument()
+        group.add_argument('missiontypes', type=argparse.FileType('r'), nargs='*', default=[], help="Mission Types")
 
 
     def shutdown_plugin(self):
